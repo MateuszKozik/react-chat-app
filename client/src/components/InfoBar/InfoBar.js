aeb249c052988558193d7bb391cbd892769b7363
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-11-02 10:48:58 +0100

    Add style file to the InfoBar component

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -3,10 +3,12 @@ import React from "react";
 import closeIcon from "../../icons/closeIcon.png";
 import onlineIcon from "../../icons/onlineIcon.png";
 
+import "./InfoBar.css";
+
 const InfoBar = ({ room }) => (
 	<div className="infoBar">
 		<div className="leftInnerContainer">
-			<img src={onlineIcon} alt="online" className="onlineIcone" />
+			<img src={onlineIcon} alt="online" className="onlineIcon" />
 			<h3>{room}</h3>
 		</div>
 		<div className="rightInnerContainer">
