0e46f5499d8faa8abc320bca497cf0711e1ccaca
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-11-02 14:11:43 +0100

    Create function to get list of users in the room

--- server/index.js
+++ server/index.js
@@ -29,6 +29,11 @@ io.on("connection", (socket) => {
 
 		socket.join(user.room);
 
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room)
+		});
+
 		callback();
 	});
 
@@ -36,6 +41,10 @@ io.on("connection", (socket) => {
 		const user = getUser(socket.id);
 
 		io.to(user.room).emit("message", { user: user.name, text: message });
+		io.to(user.room).emit("roomData", {
+			user: user.room,
+			users: getUsersInRoom(user.room)
+		});
 
 		callback();
 	});
